cad25506c69ccc2064debf6562663413e97f1c22
srsng <srsnng@hotmail.com>
2025-06-17 11:46:17 +0800

    chores

--- desks-cli/src/types/full/args.rs
+++ desks-cli/src/types/full/args.rs
@@ -9,7 +9,7 @@ use crate::types::commands::Commands;
 Windows平台下修改Desktop库目标文件夹, 使用符号链接指向已有的文件夹, 动态修改桌面内容
 注意，不保证安全，有很多实际问题没有解决。
 不支持多用户，任何手动修改桌面库位置、名称等操作都可能搞你的系统崩溃。
-Caution: this program is UNSAFE!
+Caution: this program is **UNSAFE**!
 "#
 )]
 pub struct Args {
